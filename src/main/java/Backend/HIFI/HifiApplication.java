cb0ef666777a6fc77e9a265f0b03528069ab7d75
squirMM <jsm6616@naver.com>
2022-07-21 17:26:31 +0900

    start

---
+++ src/main/java/Backend/HIFI/HifiApplication.java
@@ -0,0 +1,13 @@
+package Backend.HIFI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HifiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HifiApplication.class, args);
+	}
+
+}
