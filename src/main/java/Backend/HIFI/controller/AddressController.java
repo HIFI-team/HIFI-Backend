62c6c74a3306c55f74ef91c5e730510c26debf0b
squirMM <jsm6616@naver.com>
2022-07-24 19:31:08 +0900

    js,css ..

--- src/main/java/Backend/HIFI/controller/AddressController.java
+++ src/main/java/Backend/HIFI/controller/AddressController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AddressController {
     @GetMapping
     public String address(){
-        return "address";
+        return "thymeleaf/address";
     }
 }
