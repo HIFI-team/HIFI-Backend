0ac5db0f8be1f687b17f29cadb5876774e19b990
squirMM <jsm6616@naver.com>
2022-07-27 02:12:08 +0900

    :rocket: feat(issue): 마커관련 로직 생성 및 오류 수정중#3

--- src/main/resources/static/js/kakaoMap/mapMarker.js
+++ src/main/resources/static/js/kakaoMap/mapMarker.js
@@ -6,7 +6,8 @@ var markerPosition  = new kakao.maps.LatLng(37.5515814, 126.9249751);
 
 // 마커를 생성합니다
 var marker = new kakao.maps.Marker({
-    position: markerPosition
+    position: markerPosition,
+    clickable: true
 });
 
 // 마커가 지도 위에 표시되도록 설정합니다
