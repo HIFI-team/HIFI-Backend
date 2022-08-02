bfb2c293e4c70fc0304e13102bec49a95e25bb1d
pxxnxx <76719828+pxxnxx@users.noreply.github.com>
2022-08-03 01:15:43 +0900

    🚀 feat(user): Controller에 Follow 및 FollowList 추가 #4

--- src/main/java/Backend/HIFI/user/UserController.java
+++ src/main/java/Backend/HIFI/user/UserController.java
@@ -3,8 +3,9 @@ package Backend.HIFI.user;
 import Backend.HIFI.auth.dto.UserProfileDto;
 import Backend.HIFI.user.follow.FollowService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -14,24 +15,57 @@ public class UserController {
     private final UserService userService;
     private final FollowService followService;
 
+
+    // 아직 어떻게 처리해야 하는지 잘 몰라서 임의로 만들고 후에 다듬을 예정
     @GetMapping("/profile/{email}")
     @ResponseBody
     public String profilePage(@PathVariable("email") String email) {
         try {
-            // 본인일 프로필 볼 때도 고려해야 함
+            // TODO 본인이 프로필 볼 때 + 비공개일때 고려해야 함
             User user = userService.findByEmail(email);
             UserProfileDto userProfileDto = new UserProfileDto().toUserProfileDto(user);
-            return "abcd";
+
+            // TODO 나중에 변경 해야함
+            return userProfileDto.toString();
         } catch (IllegalArgumentException e) {
             return e.getMessage();
         }
     }
 
-    @GetMapping("/follow")
+    @GetMapping("/follow/{followerEmail}_{followingEmail}")
     @ResponseBody
-    public String followPage() {
+    public String setFollowService
+            (@PathVariable("followerEmail") String followerEmail,
+            @PathVariable("followingEmail") String followingEmail) {
 
-        return "follow";
+        try {
+            User follower = userService.findByEmail(followerEmail);
+            User following = userService.findByEmail(followingEmail);
+
+            followService.following(follower, following);
+
+            return followerEmail + " 회원이 " + followingEmail + " 회원을 팔로우했습니다.";
+
+        } catch (IllegalArgumentException e) {
+            return e.getMessage();
+        }
+
+    }
+
+
+    @GetMapping("/followList/{email}")
+    @ResponseBody
+    public String followPage(@PathVariable("email") String email) {
+        User user = userService.findByEmail(email);
+        UserProfileDto userProfileDto = new UserProfileDto().toUserProfileDto(user);
+
+        // TODO 본인이 프로필 볼 때 + 비공개일때 고려해야 함
+        List<User> followerList = userProfileDto.getFollowerList();
+        List<User> followingList = userProfileDto.getFollowingList();
+
+        // TODO 나중에 변경 해야함
+        //  followList user -> email 변경할지
+        return "followerList : " + followerList.toString() + "\n following List :" + followingList.toString();
     }
 
 //    @GetMapping("/su")
