71e3625b0e551b19b547d395901873770800d5b3
squirMM <jsm6616@naver.com>
2023-04-20 21:03:38 +0900

    :hammer: refac(comment) 댓글 서비스 로직 리팩토링 #79

--- src/main/java/Backend/HIFI/domain/comment/service/CommentService.java
+++ src/main/java/Backend/HIFI/domain/comment/service/CommentService.java
@@ -1,4 +1,15 @@
-package Backend.HIFI.domain.comment.service;/**
-* created by squirMM on {date}
-*/public interface CommentService {
+package Backend.HIFI.domain.comment.service;
+
+import Backend.HIFI.domain.comment.dto.request.PostCommentDto;
+import Backend.HIFI.domain.comment.dto.response.GetCommentDto;
+
+import java.util.List;
+
+public interface CommentService {
+    GetCommentDto createComment(Long reviewId, PostCommentDto postCommentDto, String userId);
+
+    List<GetCommentDto> getComments(Long reviewId);
+
+    void deleteComment(Long reviewId, Long id, String userId);
+
 }
