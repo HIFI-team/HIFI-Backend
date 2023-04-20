847315593d622485c27f831946265ba762bec1e7
squirMM <jsm6616@naver.com>
2023-04-21 00:42:42 +0900

    :hammer: refac(store) 가게 서비스 리팩토링 #82

--- src/main/java/Backend/HIFI/domain/store/service/StoreServiceImpl.java
+++ src/main/java/Backend/HIFI/domain/store/service/StoreServiceImpl.java
@@ -1,50 +1,62 @@
 package Backend.HIFI.domain.store.service;
 
+import Backend.HIFI.domain.store.dto.request.PostStoreDto;
+import Backend.HIFI.domain.store.dto.response.GetStoreDto;
 import Backend.HIFI.domain.store.repository.StoreRepository;
-import Backend.HIFI.domain.store.dto.StoreRequestDto;
-import Backend.HIFI.domain.store.dto.StoreResponseDto;
 import Backend.HIFI.domain.store.entity.Store;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class StoreService {
+public class StoreServiceImpl implements StoreService {
     private final StoreRepository storeRepository;
 
     /**
      * 스토어 등록
-     * */
+     */
     @Transactional
-    public StoreResponseDto registration(StoreRequestDto dto){
-        if (storeRepository.existsByUid(dto.getName())){
-            throw new RuntimeException("이미 등록된 스토어입니다");
-        }
-        Store store = dto.toEntity();
-        storeRepository.save(store);
-        return StoreResponseDto.of(store);
+    public GetStoreDto createStore(PostStoreDto postStoreDto, String userId) {
+
+        Store store = Store.builder()
+                .name(postStoreDto.getName())
+                .address(postStoreDto.getAddress())
+                .description(postStoreDto.getDescription())
+                .build();
+        Store saveStore = storeRepository.save(store);
+
+        return GetStoreDto.toDto(saveStore);
     }
+
     /**
      * 스토어 찾기
-     * */
-    public Store getStore(Long storeId){
+     */
+    public GetStoreDto getStore(Long storeId) {
         Store store = storeRepository.findById(storeId)
                 .orElseThrow(() -> new IllegalArgumentException("등록되지 않은 스토어 입니다"));
-        //delete store 제외 필요함
-        return store;
+
+        return GetStoreDto.toDto(store);
     }
-    public List<Store> getStores(){
-        return storeRepository.findAll();
+
+    public List<GetStoreDto> getStores() {
+        List<GetStoreDto> getStoreDtos = new ArrayList<>();
+        for (Store store : storeRepository.findAll()) {
+            GetStoreDto getStoreDto = GetStoreDto.toDto(store);
+            getStoreDtos.add(getStoreDto);
+        }
+        return getStoreDtos;
     }
+
     /**
      * 스토어 삭제
-     * */
+     */
     @Transactional
-    public void deleteStore(Long storeId){
+    public void deleteStore(Long storeId, String userId) {
         Store store = storeRepository.findById(storeId)
                 .orElseThrow(() -> new IllegalArgumentException("등록되지 않은 스토어 입니다"));
         store.updateIsDeleted();
