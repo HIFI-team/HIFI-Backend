a2897824eec8d9ebd6ecea52871ebe379042c64b
squirMM <jsm6616@naver.com>
2023-04-20 21:04:11 +0900

    :hammer: refac(comment) 댓글 컨트롤러 RESTapi 적용 #79

--- src/main/java/Backend/HIFI/domain/comment/CommentController.java
+++ src/main/java/Backend/HIFI/domain/comment/CommentController.java
@@ -1,33 +1,50 @@
 package Backend.HIFI.domain.comment;
 
-import Backend.HIFI.domain.comment.dto.CommentDto;
+import Backend.HIFI.domain.comment.dto.request.PostCommentDto;
+import Backend.HIFI.domain.comment.dto.response.GetCommentDto;
 import Backend.HIFI.domain.comment.service.CommentService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Slf4j
 @Controller
-@RequestMapping(value = "/comment", produces = "application/json; charset=utf-8")
+@RequestMapping(value = "reviews/{reviewId}/comments")
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
 
-    /**코멘트 등록*/
+    /**
+     * 코멘트 등록
+     */
     @ApiOperation("코멘트 등록 요청")
-    @PostMapping("/new")
-    public ResponseEntity<CommentDto> create(@RequestBody CommentDto dto){
-        return ResponseEntity.ok(commentService.comment(dto));
+    @PostMapping("/")
+    public ResponseEntity<GetCommentDto> postComment(@PathVariable("reviewId") Long reviewId, @RequestBody PostCommentDto postCommentDto, @AuthenticationPrincipal String userId) {
+        return ResponseEntity.ok(commentService.createComment(reviewId, postCommentDto, userId));
     }
 
-    /**코멘트 삭제*/
+    /**
+     * 코멘트 조회
+     */
+    @ApiOperation("코멘트 조회 요청")
+    @GetMapping("/")
+    public ResponseEntity<List<GetCommentDto>> getComments(@PathVariable("reviewId") Long reviewId) {
+        return ResponseEntity.ok(commentService.getComments(reviewId));
+    }
+
+    /**
+     * 코멘트 삭제
+     */
     @ApiOperation("코멘트 삭제 요청")
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity delete(@PathVariable Long id){
-        commentService.deleteComment(id);
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteComment(@PathVariable("reviewId") Long reviewId, @PathVariable("id") Long id, @AuthenticationPrincipal String userId) {
+        commentService.deleteComment(reviewId, id, userId);
         return ResponseEntity.ok("success");
     }
 }
