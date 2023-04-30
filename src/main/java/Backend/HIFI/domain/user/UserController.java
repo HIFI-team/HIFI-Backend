6ec45d2c51e39767a9776597ff2c22cb68372be3
pxxnxx <76719828+pxxnxx@users.noreply.github.com>
2023-04-30 17:36:46 +0900

    🔨 refac(user/table) UserController Swagger 변경중 #80

--- src/main/java/Backend/HIFI/domain/user/UserController.java
+++ src/main/java/Backend/HIFI/domain/user/UserController.java
@@ -4,17 +4,15 @@ import Backend.HIFI.domain.review.entity.Review;
 import Backend.HIFI.domain.user.dto.SearchDto;
 import Backend.HIFI.domain.user.dto.UserProfileDto;
 import Backend.HIFI.domain.user.entity.User;
-import Backend.HIFI.domain.user.entity.UserProfile;
 import Backend.HIFI.domain.user.repository.UserProfileRepository;
 import Backend.HIFI.domain.user.service.UserProfileService;
 import Backend.HIFI.domain.user.service.UserService;
-import Backend.HIFI.global.common.redis.RedisService;
 import Backend.HIFI.global.common.response.CommonApiResponse;
-import Backend.HIFI.domain.follow.repository.FollowRepository;
-import Backend.HIFI.domain.follow.service.FollowService;
 
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -24,43 +22,35 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
+@Api(tags = "유저")
 public class UserController {
 
     private final UserService userService;
     private final UserProfileService userProfileService;
-    private final FollowService followService;
-    private final FollowRepository followRepository;
-    private final RedisService redisService;
-    private final UserProfileRepository userProfileRepository;
 
-    @ApiOperation(value = "마이프로필 요청")
     @GetMapping("/profile")
+    @Operation(summary = "마이프로필 조회 요청", description = "마이프로필 조회 요청 API 입니다.")
     public CommonApiResponse<UserProfileDto> profilePage(Authentication auth) {
-        List<UserProfile> list = userProfileRepository.findAll();
-        System.out.println("!!!!!!!!!!!!!!!!");
-        for (UserProfile userProfile : list) {
-            System.out.println(userProfile.getUserId());
-        }
-        System.out.println("????????????????");
         return CommonApiResponse.of(userProfileService.getMyProfilePage(auth));
     }
 
-    @ApiOperation(value = "프로필 요청")
     @PostMapping("/profile")
+    @Operation(summary = "타인 프로필 조회 요청", description = "타인의 프로필 조회 요청 API 입니다.")
     public CommonApiResponse<UserProfileDto> profilePage(@RequestBody String email, Authentication auth) {
         return CommonApiResponse.of(userProfileService.getProfilePage(auth, email));
     }
 
-    @ApiOperation(value = "프로필 업데이트 요청")
+    // TODO @PutMapping 변경 필요
     @PostMapping("/update")
+    @Operation(summary = "프로필 업데이트 요청", description = "프로필 업데이트 요청 API 입니다.")
     public CommonApiResponse<String> updateProfile(@RequestBody UserProfileDto userProfileDto, Authentication auth) {
         userProfileService.updateProfile(auth, userProfileDto);
         return CommonApiResponse.of("프로필 업데이트 완료");
     }
 
-//
-    @ApiOperation(value = "회원 탈퇴")
+    // TODO @DeleteMapping 변경 필요
     @GetMapping("/delete")
+    @Operation(summary = "유저 탈퇴 요청", description = "유저 탈퇴 요청 API 입니다.")
     public String deletePage(Authentication auth) {
         User user = userService.findUserByAuth(auth);
         userService.deleteUser(user);
@@ -68,25 +58,22 @@ public class UserController {
         return user.getEmail() + " was deleted";
     }
 
-    // TODO Follow 분리후 처리 필요
-    @ApiOperation(value = "모든 유저 반환")
     @GetMapping("/search")
+    @Operation(summary = "모든 유저 조회 요청", description = "검색에 사용되는 모든 유저 조회 요청 API 입니다.")
     public CommonApiResponse<List<UserProfileDto>> allUserSearch(Authentication auth) {
         List<UserProfileDto> allUserProfileList = userProfileService.searchAllUserProfile(auth);
         return CommonApiResponse.of(allUserProfileList);
     }
 
-    // TODO Follow 분리 후 처리 필요
-    @ApiOperation(value = "유저 검색")
     @PostMapping("/search")
+    @Operation(summary = "특정 유저 조회 요청", description = "검색 조건에 부합하는 특정 유저 조회 요청 API 입니다.")
     public CommonApiResponse<List<UserProfileDto>> setUserSearch(@RequestBody SearchDto searchDto, Authentication auth) {
         List<UserProfileDto> searchUserProfileDtoList = userProfileService.searchUserByName(auth, searchDto);
         return CommonApiResponse.of(searchUserProfileDtoList);
     }
 
-    // TODO review Repository 사용할 것
-    @ApiOperation(value = "리뷰 리스트 반환")
     @GetMapping("/review")
+    @Operation(summary = "본인 리뷰 반환 요청", description = "본인의 리뷰 조회 요청 API 입니다.")
     public CommonApiResponse<List<Review>> getReviewList(Authentication auth) {
         List<Review> reviewList = userProfileService.getReviewListFromUser(auth);
         return CommonApiResponse.of(reviewList);
