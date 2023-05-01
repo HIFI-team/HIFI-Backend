49ee6d2261c3f3d3d9640a759482b4729afdb3c3
김민석 <kmin9811@naver.com>
2023-05-01 10:24:23 +0900

    🔨 refac(user/table) FollowController Swagger 변경 #80

--- src/main/java/Backend/HIFI/domain/follow/FollowController.java
+++ src/main/java/Backend/HIFI/domain/follow/FollowController.java
@@ -3,8 +3,11 @@ package Backend.HIFI.domain.follow;
 import Backend.HIFI.domain.follow.service.FollowService;
 import Backend.HIFI.domain.follow.dto.FollowRequestDto;
 import Backend.HIFI.global.common.response.CommonApiResponse;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,27 +16,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/follow")
 @RequiredArgsConstructor
+@Api(tags = "팔로우")
 public class FollowController {
 
     private final FollowService followService;
 
-    @ApiOperation(value = "팔로우 요청")
+
     @PostMapping("/following")
+    @Operation(summary = "팔로우 요청", description = "팔로우 요청 API 입니다.")
     public CommonApiResponse<String> followUser(@RequestBody FollowRequestDto followRequestDto) {
         followService.requestFollow(followRequestDto);
         return CommonApiResponse.of("팔로우 완료");
     }
 
-    @ApiOperation(value = "언팔로우 요청")
+    // TODO @DeleteMapping 변경
     @PostMapping("/unfollowing")
+    @Operation(summary = "언팔로우 요청", description = "언팔로우 요청 API 입니다.")
     public CommonApiResponse<String> unfollowUser(@RequestBody FollowRequestDto followRequestDto) {
         followService.requestUnFollow(followRequestDto);
         return CommonApiResponse.of("언팔로우 완료");
     }
 
-//    @ApiOperation(value = "팔로우 리스트 요청")
-//    @PostMapping("/followList")
-//    public String followPage(@RequestBody String email, Authentication auth) {
+    @PostMapping("/followList")
+    @Operation(summary = "팔로우 리스트 요청", description = "유저의 팔로워/팔로잉 리스트 요청 API 입니다.")
+    public String followPage(@RequestBody String email, Authentication auth) {
 //        User user = userService.findByEmail(email);
 //
 //        // TODO 프로필 보이도록 코드 추가 해야함
@@ -45,5 +51,6 @@ public class FollowController {
 //        List<User> followingList = followService.getFollowing(user);
 //
 //        return "followerList = " + followerList + "\nfollowingList = " + followingList;
-//    }
+        return null;
+    }
 }
